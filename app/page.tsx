70dc39e0237bfdf99e4a2d7a69f87ef0dec67f92
AliakseiLoi-SteinPilz <aliaksei.loi@stein-pilz.com>
2024-08-29 17:20:21 +0300

    fix typo

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,10 @@ export default function Home() {
         <div className="mb-5">Loyal Generation</div>
         <Cover className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
           <div className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
-            <span className="">Soming soon!</span>
+            <span className="">Coming soon!</span>
           </div>
           <div className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
-            <span className="">Soming soon!</span>
+            <span className="">Coming soon!</span>
           </div>
         </Cover>
       </h2>
